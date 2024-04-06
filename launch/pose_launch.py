b19d0b7a66a8254adab72cc408dc2f3236c99f38
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-04-06 22:49:52 +0200

    add ros2 repo

--- launch/pose_launch.py
+++ launch/pose_launch.py
@@ -4,12 +4,11 @@ from simple_launch import SimpleLauncher
 def generate_launch_description():
     
     sl = SimpleLauncher(use_sim_time=False)
-            
-    sl.declare_arg('namespace', default_value='bluerov2')
+    ns = sl.declare_arg('namespace', default_value='bluerov2')
     
-    with sl.group(ns=sl.arg('namespace')):
+    with sl.group(ns=ns):
         
         sl.node('slider_publisher','slider_publisher',
-                arguments = [sl.find('auv_control_teleop', 'pose_setpoint.yaml')])
+                arguments = [sl.find('auv_control', 'pose_setpoint.yaml')])
 
     return sl.launch_description()
--- launch/wrench_launch.py
+++ launch/wrench_launch.py
@@ -5,9 +5,9 @@ def generate_launch_description():
     
     sl = SimpleLauncher(use_sim_time = False)
     
-    sl.declare_arg('namespace', default_value='bluerov2')
+    ns = sl.declare_arg('namespace', default_value='bluerov2')
     
-    with sl.group(ns=sl.arg('namespace')):
+    with sl.group(ns=ns):
         sl.node('slider_publisher', 'slider_publisher', name='wrench_control',
                 arguments=[sl.find('auv_control', 'wrench.yaml')])
         
